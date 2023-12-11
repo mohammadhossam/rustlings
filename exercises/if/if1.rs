ab051a615a2013e38369c93ab97c7f2312eca759
Mohammad Abdulmoneim <mabubeih@gmail.com>
2023-12-11 21:49:11 +0200

    Test 1

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -2,7 +2,7 @@
 //
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-
+// Just trying the new scm tool
 pub fn bigger(a: i32, b: i32) -> i32 {
     // Complete this function to return the bigger number!
     // Do not use:
